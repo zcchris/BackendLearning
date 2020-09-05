1deedc85bcc63ef4b56785fa93c4623b006afda0
zzc <zcchen0228@gmail.com>
2020-09-04 19:57:17 -0500

    update

---
+++ SpringLearning/Spring_Basic/annoInjection/src/main/java/com/example/annoInjection/AnnoInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.annoInjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnoInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnoInjectionApplication.class, args);
+	}
+
+}
---
+++ SpringLearning/Spring_Basic/annoInjection/src/test/java/com/example/annoInjection/AnnoInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.annoInjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnoInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
